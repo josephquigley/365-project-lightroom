85e040b67c4d80b37548e7f9434c4659ee7b5bfe
Joseph Quigley <jquigley@digitallyuseful.com>
2026-04-21 12:40:55 -0400

    feat(plugin): add Info.lua manifest and menu entry

---
+++ 365Calendar.lrplugin/Info.lua
@@ -0,0 +1,16 @@
+return {
+  LrSdkVersion = 10.0,
+  LrSdkMinimumVersion = 6.0,
+
+  LrToolkitIdentifier = "com.threesixtyfiveproject.calendar",
+  LrPluginName = LOC "$$$/365Calendar/PluginName=365 Project Calendar",
+
+  LrLibraryMenuItems = {
+    {
+      title = LOC "$$$/365Calendar/MenuItem=Show 365 Calendar...",
+      file  = "ShowCalendarDialog.lua",
+    },
+  },
+
+  VERSION = { major = 0, minor = 1, revision = 0, build = 1 },
+}
